05096eeccddc52cc0546f3d97d70f32d2761917f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-10 22:18:55 +0100

    Change the function signature

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func serve(app *application) error {
+func (app *application) serve() error {
 	server := http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
