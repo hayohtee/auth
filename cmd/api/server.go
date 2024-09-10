18ba81cd003869265190c039f676e788d7a2cc53
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-10 22:18:05 +0100

    Create server.go

---
+++ cmd/api/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func serve(app *application) error {
+	server := http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	app.logger.Info("starting server", slog.String("addr", server.Addr))
+
+	return server.ListenAndServe()
+}
