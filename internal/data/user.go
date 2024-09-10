04625e6670bf779b887af78a880554b180308f53
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-10 21:41:35 +0100

    Create User struct and helper functions for validating the struct

---
+++ internal/data/user.go
@@ -0,0 +1,82 @@
+package data
+
+import (
+	"errors"
+	"github.com/hayohtee/auth/internal/validator"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+// User is a struct that holds the information about a user.
+type User struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// password is a struct which contain the plaintext and hashed
+// versions of the password for a user. The plaintext is a pointer
+// to a string, so that we are able to distinguish between password
+// not present in the struct versus a plaintext password which is empty string "".
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+// Set method calculates the bcrypt hash of a plaintext password, and
+// stores both the hash and the plaintext versions in the struct.
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+// Matches method checks whether the provided plaintext password matches
+// the hashed password stored in the struct, returning true if it matches
+// and false otherwise.
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlainText(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlainText(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
