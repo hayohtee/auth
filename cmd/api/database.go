d78ae9b9635208dca8d5c8b83078107b5b7c0c02
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-10 22:12:45 +0100

    Add database.go

---
+++ cmd/api/database.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"time"
+)
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the maximum number of open connections (in-use + idle) in the pool.
+	db.SetMaxOpenConns(cfg.db.maxOpenConn)
+	// Set the maximum number of idle connections in the pool.
+	db.SetMaxIdleConns(cfg.db.maxIdleConn)
+
+	// Use the time.ParseDuration to parse the maxIdleTime to time.Duration.
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the maximum idle time.
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
