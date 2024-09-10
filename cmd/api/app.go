1a233ac57650b7c0fcffab41ce740362332a4586
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-10 21:58:22 +0100

    Create app.go

---
+++ cmd/api/app.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/hayohtee/auth/internal/data"
+	"log/slog"
+)
+
+// application holds the dependencies for the handlers, middlewares
+// and helpers.
+type application struct {
+	config config
+	logger *slog.Logger
+	models data.UserModel
+}
