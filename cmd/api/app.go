15258dd7f0b1681e56c4fe4c81cf1cf4df572ffa
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-10 22:12:36 +0100

    Remove config field

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -8,7 +8,6 @@ import (
 // application holds the dependencies for the handlers, middlewares
 // and helpers.
 type application struct {
-	config config
 	logger *slog.Logger
 	users  data.UserModel
 }
