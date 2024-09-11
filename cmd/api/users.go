2bf4172495c8046d3cd42a8011f3d0f21a2255ea
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-11 18:55:08 +0100

    Create loginUserHandler

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hayohtee/auth/internal/data"
 	"github.com/hayohtee/auth/internal/validator"
 	"net/http"
+	"time"
 )
 
 func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -83,5 +84,88 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	w.Write(js)
+}
+
+func (app *application) loginUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Email != "", "email", "must be provided")
+	v.Check(validator.Matches(input.Email, validator.EmailRX), "email", "must be a valid email")
+	v.Check(input.Password != "", "password", "must be provided")
+
+	if !v.Valid() {
+		response := map[string]any{
+			"error": v.Errors,
+		}
+
+		js, err := json.Marshal(&response)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write(js)
+	}
+
+	user, err := app.users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("email", "user with this email does not exist")
+			response := map[string]any{
+				"error": v.Errors,
+			}
+
+			js, err := json.Marshal(&response)
+			if err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write(js)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	token, err := generateJWT(user.ID, user.Name, user.Email)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   token,
+		Expires: time.Now().Add(24 * time.Hour),
+	})
+
+	w.WriteHeader(http.StatusOK)
+	message := map[string]string{
+		"message": "Login successful",
+	}
+
+	js, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 }
