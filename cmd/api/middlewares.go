5e3077bfab3ac24100de84eb68f18cb224a66e5c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-12 16:55:54 +0100

    Add authorize middleware

---
+++ cmd/api/middlewares.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func (app *application) authorize(role string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorizationHeader := r.Header.Get("Authorization")
+		data := strings.Split(authorizationHeader, " ")
+		if len(data) != 2 {
+			w.WriteHeader(http.StatusExpectationFailed)
+			return
+		}
+
+		payload, err := validateJWT(data[1])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if !strings.EqualFold(payload.Role, role) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
