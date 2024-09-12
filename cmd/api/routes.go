0bde4389336663d680a41bc42085ced433d484cc
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-12 17:23:40 +0100

    Add new endpoints to routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,8 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -8,6 +10,9 @@ func (app *application) routes() http.Handler {
 	// Endpoints goes here
 	mux.HandleFunc("POST /users", app.createUserHandler)
 	mux.HandleFunc("POST /users/login", app.loginUserHandler)
+
+	mux.Handle("GET /admin", app.authorize(RoleAdmin, app.welcomeAdminHandler))
+	mux.Handle("GET /customer", app.authorize(RoleCustomer, app.welcomeCustomerHandler))
 
 	return mux
 }
