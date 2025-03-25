2e8a6e5ea93f29a752abac2f9f6aa9cfbf1aad65
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-25 21:48:00 +0100

    Load .env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		logger.Error("failed to load .env file", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
 }
