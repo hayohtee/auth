cf44149979d6dc9f9c23dd12150eea6c90157215
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-25 21:50:42 +0100

    Read the value for port and database dsn from commandline flags

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"os"
 
@@ -15,4 +16,9 @@ func main() {
 		logger.Error("failed to load .env file", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
+	
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "The port to start the server on")
+	flag.StringVar(&cfg.dsn, "dsn", os.Getenv("DATABASE_DSN"), "The database DSN")
+	
 }
