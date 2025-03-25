938fb18ef0de5a593a046f5ecc16a6468294425f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-25 22:01:56 +0100

    Start the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"flag"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"time"
 
@@ -17,22 +19,38 @@ func main() {
 
 	// Load .env file
 	if err := godotenv.Load(); err != nil {
-		logger.Error("failed to load .env file", slog.String("error", err.Error()))
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "The port to start the server on")
 	flag.StringVar(&cfg.dsn, "dsn", os.Getenv("DATABASE_DSN"), "The database DSN")
-	
+
 	logger.Info("opening database connection")
 	db, err := openDB(cfg.dsn)
 	if err != nil {
-		logger.Error("failed to open database connection", slog.String("error", err.Error()))
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
 	defer db.Close()
 
+	app := application{
+		config: cfg,
+	}
+
+	logger.Info("starting server", slog.String("addr", fmt.Sprintf(":%d", cfg.port)))
+	srv := http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      app.routes(),
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
 
 func openDB(dsn string) (*sql.DB, error) {
