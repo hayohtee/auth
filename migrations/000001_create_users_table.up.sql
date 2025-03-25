1ce97e4e69449dc959ea19b1e48133d07c8a8d0f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-25 22:48:33 +0100

    Add sql migration files for creating and dropping users table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users(
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT now(),
+    name TEXT NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password bytea,
+    activated bool NOT NULL
+);
