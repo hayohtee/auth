7b907f5bab2cd66aed32c9d579d98134157e1358
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-25 22:31:52 +0100

    Add new endpoints

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,5 +5,8 @@ import "net/http"
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	
+	mux.HandleFunc("GET /v1/auth/signup/oauth/google", app.signUpWithGoogle)
+	mux.HandleFunc("GET /v1/auth/signup/oauth/google/callback", app.signUpWithGoogleCallback)
+	
 	return mux
 }
