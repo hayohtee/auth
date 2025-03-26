d3e925d88769d167f1b705d7f136e9a5de5b3b7d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-26 16:47:45 +0100

    Create password struct and its associated methods

--- internal/data/user.go
+++ internal/data/user.go
@@ -1,10 +1,45 @@
 package data
 
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	ID            int64    `json:"id"`
-	Name          string   `json:"name"`
-	Email         string   `json:"email"`
-	Password      password `json:"-"`
-	EmailVerified bool     `json:"email_verified"`
-	AvatarURL     string   `json:"avatar_url,omitempty"`
+	ID            int64     `json:"id"`
+	Name          string    `json:"name"`
+	Email         string    `json:"email"`
+	Password      password  `json:"-"`
+	EmailVerified bool      `json:"email_verified"`
+	AvatarURL     string    `json:"avatar_url,omitempty"`
+	CreatedAt     time.Time `json:"-"`
+}
+
+type password struct {
+	PlainText string
+	Hash      []byte
+}
+
+func (p *password) Set(plainTextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plainTextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.PlainText = plainTextPassword
+	p.Hash = hash
+	return nil
+}
+
+func (p *password) Matches(plainTextPassword string) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword(p.Hash, []byte(plainTextPassword)); err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
 }
