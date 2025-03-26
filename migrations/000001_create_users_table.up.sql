fd295ca18edf5030eca1d56e7c650ea7eba2a8e7
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-26 17:15:42 +0100

    Change password to password_hash

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users(
     id bigserial PRIMARY KEY,
     name text NOT NULL,
     email citext NOT NULL,
-    password bytea,
+    password_hash bytea,
     email_verified bool NOT NULL,
     avatar_url text,
     created_at timestamp(0) with time zone NOT NULL DEFAULT now()
