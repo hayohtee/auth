2a7006e376d2ba4de2b3b5bdc0f499fefeb17dc3
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-28 15:20:05 +0100

    Update user_model.go

--- internal/data/user_model.go
+++ internal/data/user_model.go
@@ -3,7 +3,6 @@ package data
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"time"
 )
 
@@ -11,50 +10,30 @@ type UserModel struct {
 	db *sql.DB
 }
 
-func (u UserModel) Insert(user *User) error {
+func (u UserModel) Insert(user *UserWithCredential) error {
 	query := `
-		INSERT INTO users(name, email, email_verified, password_hash)
-		VALUES($1, $2, $3, $4)
-		RETURNING id, created_at`
+		WITH new_user AS (
+			INSERT INTO users(name)
+			VALUES($1)
+			RETURNING id, created_at
+		)
+		INSERT INTO user_credentials(user_id, email, password_hash)
+		SELECT n.id, $2, $3 FROM n new_user
+		RETURNING n.id, email, n.created_at
+		`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	args := []any{user.Name, user.Email, user.EmailVerified, user.Password.Hash}
-	if err := u.db.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (u UserModel) GetByEmail(email string) (User, error) {
-	query := `
-		SELECT id, name, email, email_verified, password_hash, avatar_url, created_at
-		FROM users
-		WHERE id = $1 AND password_hash IS NOT NULL`
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	var user User
-	err := u.db.QueryRowContext(ctx, query, email).Scan(
-		&user.ID,
-		&user.Name,
-		&user.Email,
-		&user.EmailVerified,
-		&user.Password.Hash,
-		&user.AvatarURL,
-		&user.CreatedAt,
-	)
-
+	args := []any{user.User.Name, user.Credential.Email, user.Credential.password.Hash}
+	err := u.db.QueryRowContext(ctx, query, args...).Scan(&user.User.ID, &user.User.Email, &user.User.CreatedAt)
 	if err != nil {
 		switch {
-		case errors.Is(err, sql.ErrNoRows):
-			return User{}, ErrRecordNotFound
+		case err.Error() == `pq: duplicate key value violates unique constraint "user_credentials_email_key"`:
+			return ErrDuplicateEmail
 		default:
-			return User{}, err
+			return err
 		}
 	}
-	return user, nil
+	return nil
 }
