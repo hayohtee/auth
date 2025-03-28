2128d8cf1dbe78890e59ae5821ece99a905f0066
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-28 13:35:36 +0100

    Remove sql migration files

--- migrations/000001_create_users_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    id bigserial PRIMARY KEY,
-    name text NOT NULL,
-    email citext NOT NULL,
-    password_hash bytea,
-    email_verified bool NOT NULL,
-    avatar_url text,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT now()
-);
