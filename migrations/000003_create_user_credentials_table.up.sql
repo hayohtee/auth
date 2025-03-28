ea07d261ac98e187b736074c184377c9548a7c43
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-28 13:57:57 +0100

    Add user_id column

--- migrations/000003_create_user_credentials_table.up.sql
+++ migrations/000003_create_user_credentials_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_credentials (
     id bigserial PRIMARY KEY,
+    user_id bigserial REFERENCES users (id) ON DELETE CASCADE,
     email citext UNIQUE NOT NULL,
     password_hash bytea NOT NULL
 );
