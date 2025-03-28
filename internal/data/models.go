0c43d1ec309e0b6a74a764a075b461be1a5471b6
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-28 15:20:23 +0100

    Update models.go

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,15 +3,13 @@ package data
 import "database/sql"
 
 type Models struct {
-	Users         UserModel
-	AuthProviders UserAuthProviderModel
-	Tokens        StateTokenModel
+	Users  UserModel
+	Tokens StateTokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Users:         UserModel{db: db},
-		AuthProviders: UserAuthProviderModel{db: db},
-		Tokens:        StateTokenModel{db: db},
+		Users:  UserModel{db: db},
+		Tokens: StateTokenModel{db: db},
 	}
 }
