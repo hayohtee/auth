87b51f01d5bed13d935c0d356532747b7832440c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-03-28 22:21:59 +0100

    Update User struct field

--- internal/data/user.go
+++ internal/data/user.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"database/sql"
+	"encoding/json"
 	"errors"
 	"time"
 
@@ -9,11 +11,22 @@ import (
 )
 
 type User struct {
-	ID        int64     `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	AvatarURL string    `json:"avatar_url,omitempty"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int64      `json:"id"`
+	Name      string     `json:"name"`
+	Email     string     `json:"email"`
+	AvatarURL nullString `json:"avatar_url"`
+	CreatedAt time.Time  `json:"created_at"`
+}
+
+type nullString struct {
+	sql.NullString
+}
+
+func (n nullString) MarshalJSON() ([]byte, error) {
+	if n.Valid {
+		return json.Marshal(n.String)
+	}
+	return []byte("null"), nil
 }
 
 type UserWithCredential struct {
